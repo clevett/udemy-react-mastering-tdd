a158b7bc41124f9f4b45e5b09247df0383bbcfc8
Cassie <cassielevett@gmail.com>
2020-10-10 11:51:09 -0500

    add first button'

--- giftgiver/src/components/App/App.js
+++ giftgiver/src/components/App/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			gifts: []
+		}
+	}
+
+	addGift = () => {
+		let { gifts } = this.state
+		const ids = this.state.gifts.map(gift => gift.id)
+		const maxID = ids.lenght > 0 ? Math.max(...ids) : 0
+		gifts.push({ id: maxID + 1})
+		this.setState({ gifts })
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Gift Giver</h1>
+				<Button className='btn-add' onClick={this.addGift}>Add Gift</Button>
 			</div>
 		)
 	}

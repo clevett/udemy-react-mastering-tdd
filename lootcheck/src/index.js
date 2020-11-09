b5d9b001a13b695f230c1394fa50ae6f79b77a11
Cassie <cassielevett@gmail.com>
2020-11-09 16:27:04 -0600

    section 4

--- lootcheck/src/index.js
+++ lootcheck/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from "react-dom"
 import App from './components/App/App'
-import { createStore } from 'redux'
-import rootReducer from "./reducers/balance"
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from "./reducers"
 import { Provider } from 'react-redux'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 render(
 	<Provider store={store}><App /></Provider>, 
